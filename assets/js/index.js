9674bc83c390d8b4eae6fc811bf25f17d7a4b7a2
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-02-17 06:04:46 +0600

    event listener working

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
-document.getElementById("calculate-button").addEventListener("click", function() { alert("Hello World!"); });
+function valueParsing(input) {
+    value = parseFloat(document.getElementById(input + "-input").value);
+    return value;
+
+}
+
+
+document.getElementById("calculate-button").addEventListener("click", function() {
+
+    income = valueParsing("income");
+    food = valueParsing("food");
+    rent = valueParsing("rent");
+    cloth = valueParsing('cloth');
+    save = valueParsing("save");
+
+    // console.log(income)
+    // console.log(food)
+    // console.log(rent)
+    // console.log(cloth)
+    // console.log(save)
+
+
+});
